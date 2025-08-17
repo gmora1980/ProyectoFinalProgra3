a82106fdcec8bc7b3ae3be08a09ec88f5d84e7fb
gusmorji <165494334+gusmorji@users.noreply.github.com>
2025-08-16 20:22:06 -0600

    Añadir control ViewSwitcher y lógica de cierre de sesión
    Se han realizado modificaciones en `Site.Master` para incluir un control de usuario `ViewSwitcher` y actualizar el enlace de la marca de la aplicación a la página de inicio de sesión. Se eliminaron enlaces de navegación y se añadió un enlace para "Login". En `Site.Master.designer.vb`, se generaron nuevos controles para `lnkHome`, `ViewSwitcher` y `lnkLogin`. Además, se implementó el método `CerrarSesion_Click` en `Site.Master.vb` para gestionar el cierre de sesión. Por último, se añadió una cadena de conexión en `Web.config` para la base de datos.

--- Site.Master.vb
+++ Site.Master.vb
@@ -3,4 +3,11 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
 
     End Sub
+    Protected Sub CerrarSesion_Click(ByVal sender As Object, ByVal e As EventArgs)
+        ' Cierra sesión al hacer clic en "Login" o "Examen Stephanie"
+        Session.Clear()
+        Session.Abandon()
+        Response.Redirect("login.aspx")
+    End Sub
+
 End Class
